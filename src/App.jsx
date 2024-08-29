bc018a444e1e2f5f36b8f7b9224961154bd43f1c
Gareth Dean <gazdean@hotmail.com>
2024-08-29 23:57:59 +0100

    basic layout of buttons

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,47 @@
-import { useState } from 'react'
+import { useState } from 'react';
 import './App.css'
+import Button from './components/Button'
 
-function App() {
+
+export default function App() {
+  const [shown, setShown] = useState(0)
+  const [calculation, setCalculation] = useState(0)
+  const [numString, setNumString] = useState('')
+
+  const renderButtons = () => {
+    const buttons = []
+    const symbols = {
+      add: '➕', 
+      subtract: '➖',
+      multiply: '✖️',
+      divide: '➗', 
+      percent: '％', 
+      equals: '🟰', 
+      decimalPoint: '.',
+      allClear: 'AC',
+      plusMinus: '+/-'
+     }
+
+    let count = 0
+    for(const symbol in symbols) {
+      const text = symbols[symbol]
+      count ++
+      buttons.push(<Button key={`symbol${count}`} id={symbol} text={text} type={'symbol'} setShown={setShown} setNumString={setNumString}/>)
+    }
+
+    for (let i = 9; i >= 0; i--) {
+      buttons.push(<Button key={`number${i}`} id={i} text={i} type={'number'} setShown={setShown} setNumString={setNumString}/>);
+    }
+
+    return buttons;
+  };
+
   return (
-    <>
-    <h1>HEY</h1>
-    </>
-  )
+    <div id="calculator-body">
+      <div id="calculator-screen">{shown}</div>
+      <div id="number-button-container">
+        {renderButtons()}
+      </div>
+    </div>
+  );
 }
-
-export default App
